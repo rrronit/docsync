67b64b23a4b2fd818158aea680518b48eaec9752
rrronit <ronitsingh.cool4@gmail.com>
2023-11-11 03:29:40 +0530

    socket setup complete

--- client/src/Component/Editor.tsx
+++ client/src/Component/Editor.tsx
@@ -1,19 +1,54 @@
 "use client"
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useQuill } from 'react-quilljs';
-import 'quill/dist/quill.snow.css'; // Import Quill styles
+import 'quill/dist/quill.snow.css';
+import { io } from 'socket.io-client';
+import { RangeStatic } from 'quill';
 
 const Editor = () => {
-  const { quill, quillRef } = useQuill({theme:"snow",});
+  const { quill, quillRef } = useQuill({ theme: 'snow' });
+  const socket = io('http://192.168.87.171:4000');
+  let cursor:any;
 
   useEffect(() => {
+    const handleTextChange = (delta:any, oldDelta:any, source:any) => {
+     
+        cursor=quill?.getSelection()
+        console.log(delta)
+
+        socket.emit('text-change', quill?.getContents());
+    
+    };
+
     if (quill) {
-      
+      quill.on('text-change', handleTextChange);
+
+
+      socket.on('some-change-text', (data) => {
+        // Temporarily disable 'text-change' event handling while applying changes received from the server
+        quill.off('text-change', handleTextChange);
+
+
+        // Apply the received content
+        quill.setContents(data);
+
+        quill?.setSelection(cursor)
+
+        // Re-enable 'text-change' event handling
+        quill.on('text-change', handleTextChange);
+      });
     }
-  }, [quill]); // Make sure to include quill as a dependency to avoid unnecessary re-renders
+
+    return () => {
+      if (quill) {
+        quill.off('text-change', handleTextChange);
+      }
+      socket.disconnect();
+    };
+  }, [quill]);
 
   return (
-    <div className='' style={{ height:"100vh", width:"80vw",backgroundColor: 'white',color:"black" }}>
+    <div>
       <div ref={quillRef} />
     </div>
   );
--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
 import http from "http"
+import cors from "cors"
 import socket from "socket.io"
 dotenv.config()
 
+
 const app=express()
 const Server=new http.Server(app)
-const socketIO=new socket.Server(Server)
+const socketIO=new socket.Server(Server,{cors:{origin:["http://192.168.87.171:3001","http://localhost:3001"],credentials:true}})
 const PORT=4000;
+app.use(cors({origin:["http://192.168.87.171:3001/","http://localhost:3001"],credentials:true}))
 
+socketIO.on("connection",socket=>{
+    socket.join("1")
+    socket.on("text-change",data=>{
+        socket.to("1").emit("some-change-text",data)
+    })
+})
 
 
 

6cd6e857c896041a47a2ec6a3d1a535a4792722b
Ronit <Ronitsingh.cool4@gmail.com>
2023-11-18 20:11:44 +0530

    Fix redis undefined

--- client/src/app/api/document/route.ts
+++ client/src/app/api/document/route.ts
@@ -1,9 +1,16 @@
 import prisma from "@/lib/prisma";
 import { randomUUID } from "crypto";
+import { Redis } from "@upstash/redis";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST=async(req:NextRequest)=>{
-
+    let redis;
+    if (process.env.REDIS_TOKEN && process.env.REDIS_URL) {
+      redis = new Redis({
+        url: process.env.REDIS_URL,
+        token: process.env.REDIS_TOKEN,
+      });
+    }
     const {title}=await req.json()
     const doc=await prisma.document.create({data:{
         id:randomUUID() as string,
